70508b458df5205fbdc51f8b2acde5ccdaa89d2b
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-01-16 17:31:14 +1100

    .

---
+++ modal-mockup.js
@@ -0,0 +1,25 @@
+console.log("hello world");
+
+var modal = document.getElementById("ask-fullscreen-modal");
+
+var modalBtn = document.getElementById("openModalBtn");
+
+const fullscreenYesBtn = document.getElementById("fullscreen-yes-btn");
+
+modalBtn.addEventListener("click", openModal);
+fullscreenYesBtn.addEventListener("click", fullscreenYesHandler);
+
+///// fns
+
+function openModal() {
+  // modal.style.display = "block";
+
+  modal.classList.remove("hide");
+
+}
+
+function fullscreenYesHandler() {
+  console.log("yes handler");
+
+  document.documentElement.requestFullscreen();
+}
