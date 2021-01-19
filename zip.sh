4aa48dcd65633333526dcfe80c28881c02668132
Thomas Berghuis <thomas.berghuis@gmail.com>
2021-01-19 14:06:33 +1100

    .

--- zip.sh
+++ zip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-
+rm tv-for-youtube.zip
 zip tv-for-youtube.zip background.js  content.css  content.js   manifest.json fontawesome/all.css fontawesome/webfonts/fa-solid-900.woff2 icons/icon-128.png icons/icon-48.png icons/icon-16.png
 
